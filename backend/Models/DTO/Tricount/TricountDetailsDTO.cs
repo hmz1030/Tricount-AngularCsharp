cec1260a9d1fa314f76a15cfa0bebd391323e9e7
Mohamed El Hajioui <mohamedelhajioui3@gmail.com>
2025-11-17 22:25:38 +0100

    feat(get_my_tricounts): implementation de l'endpoint get_my_tricounts

---
+++ backend/Models/DTO/Tricount/TricountDetailsDTO.cs
@@ -0,0 +1,14 @@
+using Tricount.Models.DTO.User;
+
+namespace Tricount.Models.DTO.Tricount;
+
+public class TricountDetailsDTO 
+{
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string? Description { get; set; }
+        public DateTime Created_at { get; set; }
+        public int Creator { get; set; }
+        public List<UserDTO> Participants { get; set; } = new();
+        public List<OperationDTO> Operations { get; set; } = new();
+}
