9dd0371e1cbd03b4c2d31cc86dc234cef839aa11
Mohamed El Hajioui <mohamedelhajioui3@gmail.com>
2025-11-25 19:06:09 +0100

    fix: suppression des composants de base

--- frontend/src/app/components/app/app.component.ts
+++ frontend/src/app/components/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavMenuComponent } from '../nav-menu/nav-menu.component';
+
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     standalone: true,
-    imports: [NavMenuComponent, RouterOutlet]
+    imports: [RouterOutlet]
 })
 export class AppComponent {
     title = 'app';
