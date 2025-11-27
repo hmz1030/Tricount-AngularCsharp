893611cb514bc3306c0a9b92a747810f6960165f
Hamza Stitou <hamzastitou11s@hotmail.com>
2025-11-27 01:24:08 +0100

    feat(signup) : debut du ts component sans check name & mail

---
+++ frontend/src/app/components/signup/signup.component.ts
@@ -0,0 +1,81 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators, FormControl, AsyncValidatorFn, ValidationErrors, AbstractControl, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { AuthenticationService } from '../../services/authentication.service';
+
+
+@Component({
+    selector: 'app-signup',
+    standalone: true,
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        MatCardModule,
+        MatIconModule
+    ],
+    templateUrl: './signup.component.html',
+    styleUrls: ['./signup.component.css']
+})
+export class SignupComponent {
+    public frm!: FormGroup;
+    public ctlEmail!: FormControl;
+    public ctlFullName!: FormControl;
+    public ctlPassword!: FormControl;
+    public ctlPasswordConfirm!: FormControl;
+    public ctlIban!: FormControl;
+    public hidePassword = true;
+    public hidePasswordConfirm = true;
+    constructor(
+        private fb: FormBuilder,
+        private router: Router,
+        public authService: AuthenticationService
+    ) {
+        this.ctlEmail = this.fb.control('', [Validators.required, Validators.email]);
+        this.ctlFullName = this.fb.control('', [Validators.required, Validators.minLength(3)]);
+        this.ctlPassword = this.fb.control('', [Validators.required, Validators.minLength(8)]);
+        this.ctlPasswordConfirm = this.fb.control('', [Validators.required]);
+        this.ctlIban = this.fb.control('');
+        this.frm = this.fb.group({
+            email: this.ctlEmail,
+            fullName: this.ctlFullName,
+            password: this.ctlPassword,
+            passwordConfirm: this.ctlPasswordConfirm,
+            iban: this.ctlIban
+        }, { validators: this.crossValidations });
+    }
+    crossValidations(group: AbstractControl): ValidationErrors | null {
+        const password = group.get('password');
+        const passwordConfirm = group.get('passwordConfirm');
+        return password && passwordConfirm && password.value === passwordConfirm.value
+            ? null
+            : { passwordNotConfirmed: true };
+    }
+    signup() {
+    if (this.frm.invalid) {
+      return;
+    }
+
+    const { email, password, fullName, iban } = this.frm.value;
+
+    this.authService.signup(email, password, fullName, iban || undefined).subscribe({
+      next: () => {
+        if (this.authService.currentUser) {
+          this.router.navigate(['/tricounts']);
+        }
+      },
+      error: err => {
+        console.error('Erreur signup:', err);
+      }
+    });
+  }
+    
+}
