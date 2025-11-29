45578a0660ddbd8ba4354cb3eb7bb2df329f68a0
Aly Samir <2208alsamirmahmoudib@student.epfc.eu>
2025-11-29 16:37:02 +0100

    feat(Tricount): recuperer les data grace aux services et gestion de l'html et css et creation du model operation

--- frontend/src/app/components/Tricount/tricount.component.ts
+++ frontend/src/app/components/Tricount/tricount.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 import { Tricount } from "src/app/models/Tricount";
 import { AuthenticationService } from "src/app/services/authentication.service";
 import { TricountService } from "src/app/services/tricount.service";
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from "@angular/common";
 
 @Component({
     selector: 'app-tricounts',
     standalone: true,
+        imports: [CommonModule, MatIconModule],
     templateUrl: './tricount.component.html',
     styleUrls : ['./tricount.component.css']
 })
@@ -17,11 +20,33 @@ export class TricountComponent implements OnInit{
         private tricountService : TricountService,
         private authService : AuthenticationService,
         private router: Router,
-    ){
-    };
+        private route : ActivatedRoute
+    ){};
 
 
     ngOnInit(): void {
+        const id = Number(this.route.snapshot.paramMap.get('id'));
+
+        this.tricountService.getMyTricounts().subscribe({
+            next: (tricounts) => {
+                this.tricount = tricounts.find(t => t.id == id);
+                console.log("Found Tricound : ",this.tricount)
+            },
+            error: (err) => {
+                console.error('Error loading tricount:', err);
+            }
+        })
+    }
+
+    goBack(): void{
+        this.router.navigate(['/tricounts']);
+    }
+
+    refresh(): void{
+    }
+    edit(): void{
+    }
+    delete(): void{
     }
     
 }
